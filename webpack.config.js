6cbe27beda04fcadbef1e18d8096bb6626f8c771
yasir19 <yasir.habboo@gmail.com>
2022-03-14 22:04:12 -0500

    using webpack to bundle the app functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,50 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const webpack = require('webpack');
 const path = require('path');
 module.exports = {
-    entry:'./assets/js/script.js',
+    entry:{
+        app:'./assets/js/script.js',
+        event:'./assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets:'./assets/js/tickets.js',
+
+    },
     output:{
         path:path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test:/\.jpg$/i,
+                use:[
+                    {
+                        loader:'file-loader',
+                       options:{
+                           esModule: false,
+                           name(file){
+                               return "[path][name].[text]"
+                           },
+                           publicPath: function(url) {
+                               return url.replace('../','/assets')
+                           }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ],
+            },
+        ],
     },
     plugins:[
         new webpack.ProvidePlugin({
             $:'jquery',
             jQuery: 'jquery'
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static', //the report outputs to an HTML file in the dist folder
+        })
     ],
     mode:'development'
 };
